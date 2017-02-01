5333f35a1f9eb685468749c8953440f63830df56
Santangelo Williams <santangelowilliams@Santangelos-MBP.fios-router.home>
2017-02-01 01:53:10 -0500

    updated the css and added js

--- 02/sketch.js
+++ 02/sketch.js
@@ -1,18 +1,43 @@
-function setup(){
+var ellipsePosX = new int[height];
 
 
 
-createCanvas(1800,900);
 
+
+function setup() {
+  
+  size(500,500);
+  background(0);
+  
+  for( int i = 0; i < ellipsePosX.length; i++){
+  ellipsePosX[i]=0;
+  }
+  
 }
 
-function draw(){
-noStroke();
-fill(mouseX,mouseY,mouseX)
-ellipse(mouseX,mouseY,20,20);
 
 
 
 
+function draw() {
+  
+  background(255);
+  
+   noStroke();
+  //rect(0,0,width,height);
+  
+  
+  stroke(0);
+  for (var i = 0; i < ellipsePosX.length; i++) {
+  fill (mouseX,mouseY,200);
+    ellipse(ellipsePosX[i], i * 20, 20, 20);
+  }
 
+for(var i = 0; i< ellipsePosX.length; i++){
+  
+  if (ellipsePosX[i] < width) { 
+    
+    ellipsePosX[i] += i;}
+    else
+    { ellipsePosX[i] = 0;}}
 }
