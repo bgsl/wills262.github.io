2a06ed57850a9101cd19b85df4c9cd265f798c31
Santangelo Williams <santangelowilliams@Santangelos-MBP.home>
2017-02-03 22:23:06 -0500

    trying camera

--- 02/sketch.js
+++ 02/sketch.js
@@ -1,43 +1,14 @@
-var ellipsePosX = new int[height];
-
-
-
-
+var capture;
 
 function setup() {
-  
-  size(500,500);
-  background(0);
-  
-  for( int i = 0; i < ellipsePosX.length; i++){
-  ellipsePosX[i]=0;
-  }
-  
+  createCanvas(800, 300);
+  capture = createCapture(VIDEO);
+  capture.size(320, 240);
+  //capture.hide();
 }
 
-
-
-
-
 function draw() {
-  
   background(255);
-  
-   noStroke();
-  //rect(0,0,width,height);
-  
-  
-  stroke(0);
-  for (var i = 0; i < ellipsePosX.length; i++) {
-  fill (mouseX,mouseY,200);
-    ellipse(ellipsePosX[i], i * 20, 20, 20);
-  }
-
-for(var i = 0; i< ellipsePosX.length; i++){
-  
-  if (ellipsePosX[i] < width) { 
-    
-    ellipsePosX[i] += i;}
-    else
-    { ellipsePosX[i] = 0;}}
+  image(capture, 0, 0, 320, 240);
+;
 }
