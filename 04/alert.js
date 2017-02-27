4ff89eb27577dbe225e252c6b2a88046a69a2092
Santangelo Williams <santangelowilliams@Santangelos-MBP.wireless.newschool.edu>
2017-02-27 11:23:42 -0500

    homework for week 4

--- 04/alert.js
+++ 04/alert.js
@@ -2,10 +2,14 @@ var button;
 var textarea;
 var savedText;
 
+
+;
 function setup() {
   noCanvas();
-  textarea = createElement('textarea', 'Convert Processing to Javascript');
+  textarea = createElement('textarea', "Paste the passage");
   textarea.size(300,400);
+  // textarea.fillStyle = blue;
+  // textarea.fillRect(20,20,1055,1055);
   createElement('br');
   button = createButton('push to save');
   button.mousePressed(saveText);
@@ -37,22 +41,41 @@ function saveText() {
 
 
   for (var i = 0; i < textArr.length; i++) {
-    if (textArr[i] === "int" || textArr[i] === "float") {
-      textArr[i] = "var";
+    if (textArr[i] === "he" || textArr[i] === "He") {
+      textArr[i] = "They";
+  }
+    if (textArr[i] === "GOD" || textArr[i] === "God") {
+      textArr[i] = "YOU";
     }
-    if (textArr[i] === "string" || textArr[i] === "boolean") {
-      textArr[i] = "var";
+    if (textArr[i] === "the" ) {
+      textArr[i] = "a";
     }
-    if (textArr[i] === "void" ) {
-      textArr[i] = "function";
+     if (textArr[i] === "you" ) {
+      textArr[i] = "my";
+    }
+
+        if (textArr[i] === "my" ) {
+      textArr[i] = "you";
+    }
+
+
+         if (textArr[i] === "christ" ) {
+      textArr[i] = "the other";
     }
   }
 
 
   // join the new jumbled array into a single string, separated by spaces
-   for(var p = 0; p < 5; p++) {
+   for(var p = 0; p < 100; p++) {
     var newString = join(jumbledArr, ' ');
 
   createP(newString.toUpperCase());
 }
 }
+
+
+
+
+
+
+
